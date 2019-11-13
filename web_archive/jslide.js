a2f353a6488d0e40fe7961f36f8df5231f5f87e1
Jared Powrie <jnpowrie@southern.edu>
2019-11-12 20:39:40 -0500

    old site

---
+++ web_archive/jslide.js
@@ -0,0 +1,38 @@
+$(function(){$('#inslider').css({'margin-left': page * boffset})});
+var current = 0;
+var offset = -610;
+function move(direction, location){
+	if (direction == 0) current = location;
+	else if (current == 0 && direction == -1) current = total - 1;
+	else current = (current + direction) % total;
+	$('#rotator_items').stop();
+	$('#rotator_items').animate({'margin-left': current * offset});
+}
+var boffset = -160;
+function bmove(location){
+	var leftmargin = $("#inslider").css("marginLeft");
+	if (parseInt(leftmargin) != location * boffset) {
+		$('#inslider').stop();
+		$('#inslider').animate({'margin-left': location * boffset});
+	}
+}
+//This is for preloading images and swapping them
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
